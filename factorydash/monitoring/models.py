836ebdd6b7b70b77d5ad5cf05a373036d6062647
Francisco Ricardo <franciscoricardo.dev@gmail.com>
2025-02-20 15:54:56 -0300

    Create the model MachineData
     Changes to be committed:
    	new file:   factorydash/monitoring/migrations/0001_initial.py
    	modified:   factorydash/monitoring/models.py

--- factorydash/monitoring/models.py
+++ factorydash/monitoring/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+class MachineData(models.Model):
+    DATA_TYPES = [
+        ('Events', 'Events'),
+        ('Samples', 'Samples'),
+    ]
+
+    data_type = models.CharField(max_length=10, choices=DATA_TYPES)
+    data_item_id = models.CharField(max_length=255)
+    timestamp = models.DateTimeField()
+    name = models.CharField(max_length=255, null=True, blank=True)
+    value = models.CharField(max_length=255, null=True, blank=True)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['timestamp']),
+            models.Index(fields=['data_item_id']),
+        ]
+
+    def __str__(self):
+        return f"{self.name} - {self.value} ({self.timestamp})"
