4acd912be62ab021074c3306dc76ddce959d951b
Francisco Ricardo <franciscoricardo.dev@gmail.com>
2025-02-28 16:42:26 -0300

    Keep working on the custom comm for data loading
     Changes to be committed:
    	modified:   app/factorydash/factorydash/celery.py
    	deleted:    app/factorydash/monitoring/data_loader.py
    	modified:   app/factorydash/monitoring/tasks.py

--- app/factorydash/factorydash/celery.py
+++ app/factorydash/factorydash/celery.py
@@ -12,15 +12,15 @@ app.autodiscover_tasks()
 
 # Tasks scheduling (Celery Beat)
 app.conf.beat_schedule = {
-    "fetch-nist-data-every-10-seconds": {
-        "task": "monitoring.tasks.fetch_nist_data_task",
-        "schedule": 600.0,  # Execute every 10 seconds
+    "load_nist_data_task_every_10_seconds": {
+        "task": "monitoring.tasks.load_nist_data_task",
+        "schedule": 10.0,  # Execute every 10 seconds
     },
-    "cleanup-old-data-daily-1": {
+    "cleanup_old_data_daily_1": {
         "task": "monitoring.tasks.cleanup_task",
         "schedule": crontab(hour=12, minute=5),  # Run daily at 12:05
     },
-    "cleanup-old-data-daily-2": {
+    "cleanup_old_data_daily_2": {
         "task": "monitoring.tasks.cleanup_task",
         "schedule": crontab(hour=0, minute=5),  # Run daily at 00:05
     },
--- app/factorydash/monitoring/tasks.py
+++ app/factorydash/monitoring/tasks.py
@@ -1,21 +1,20 @@
 import factorydash  # This will set up the Django environment and logging
 
 from celery import shared_task
-
-from monitoring.data_loader import save_nist_data
 from django.core.management import call_command
 
 
 @shared_task
-def fetch_nist_data_task() -> str:
+def load_nist_data_task() -> str:
     """Celery task to fetch and save data from the NIST API."""
     try:
-        save_nist_data()
-        factorydash.logger.info("Celery Task: Successfully fetched NIST API data")
-        return "NIST API data fetched and stored."
+        call_command('load_nist_data')        
     except Exception as e:
         factorydash.logger.error(f"Celery Task Error: {str(e)}")
         return "NIST API task failed."
+    else:
+        factorydash.logger.info("Celery Task: Successfully fetched NIST API data")
+        return "NIST API data fetched and stored."
 
 @shared_task
 def cleanup_task() -> str:
