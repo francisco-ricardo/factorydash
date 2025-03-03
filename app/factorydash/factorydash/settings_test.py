a0ed45d6a36c54177bf56e0dc349e6f2a68b0a28
Francisco Ricardo <franciscoricardo.dev@gmail.com>
2025-03-03 15:45:33 -0300

    Keep working on ci/cd
     Changes to be committed:
    	modified:   .github/workflows/ci.yml
    	modified:   app/factorydash/factorydash/settings_test.py
    	modified:   app/factorydash/pytest.ini

--- app/factorydash/factorydash/settings_test.py
+++ app/factorydash/factorydash/settings_test.py
@@ -1,5 +1,22 @@
+# settings_test.py (for pytest)
+
 from .settings import *
 
-# Override settings for testing
+DEBUG = True
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "factorydash",
+        "USER": "factorydash",
+        "PASSWORD": "factorydash",
+        "HOST": "localhost",
+        "PORT": "5433",  # Must match the port in ci.yml
+    }
+}
+
+CELERY_BROKER_URL = "redis://localhost:6380/0"
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
+
+# EOF
