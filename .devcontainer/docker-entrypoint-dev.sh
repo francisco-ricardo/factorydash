717c0c25c6664161d27617c729729ab06da300f0
Francisco Ricardo <franciscoricardo.dev@gmail.com>
2025-03-13 16:59:07 -0300

    Fix logs directory issue in development environment

--- .devcontainer/docker-entrypoint-dev.sh
+++ .devcontainer/docker-entrypoint-dev.sh
@@ -5,6 +5,12 @@ set -e
 # Ensure logs directory exists inside the container
 mkdir -p /factorydash/app/factorydash/logs
 
+# Run database migrations
+python /factorydash/app/factorydash/manage.py migrate
+
+# Check the integrity of the Django project
+python /factorydash/app/factorydash/manage.py check --settings=factorydash.settings_test
+
 exec "$@"
 
 # EOF
